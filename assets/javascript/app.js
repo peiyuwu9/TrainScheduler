800555439aba3b6538e35dcce908e44c4fb0204d
Kevin Wu <peiyuwu9@gmail.com>
2019-10-10 22:31:33 -0400

    Make sure it's working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
 //Access to firebase
 var config = {
-apiKey: "AIzaSyA76LVKWgZ6VBsGonlNDoN_3tKX9SF-pIo",
-authDomain: "happy-hour-a23a6.firebaseapp.com",
-databaseURL: "https://happy-hour-a23a6.firebaseio.com",
-projectId: "happy-hour-a23a6",
-storageBucket: "",
-messagingSenderId: "256506802274",
-appId: "1:256506802274:web:c5cffbc411795dbad5613b",
-measurementId: "G-FP7DSH1CGZ"
+    apiKey: "AIzaSyA76LVKWgZ6VBsGonlNDoN_3tKX9SF-pIo",
+    authDomain: "happy-hour-a23a6.firebaseapp.com",
+    databaseURL: "https://happy-hour-a23a6.firebaseio.com",
+    projectId: "happy-hour-a23a6",
+    storageBucket: "happy-hour-a23a6.appspot.com",
+    messagingSenderId: "256506802274",
+    appId: "1:256506802274:web:9acd38a52f11b543d5613b",
+    measurementId: "G-DX0YJPQ190"
 };
+
 firebase.initializeApp(config);
 
 //Set a variable to save data
@@ -16,128 +17,137 @@ var database = firebase.database();
 
 var trainData = database.ref("/train");
 
+console.log(trainData);
 
 
-//Once users click button, input information will be submitted
-$("#submit-button").on("click",function(event){
+$(document).ready(function() {
 
-    //Prevent inputs being automatically submitted
-    event.preventDefault();
+    //Once users click button, input information will be submitted
+    $("#submit-button").on("click",function(event){
 
-    //Retrive values from inputs
-    var trainNameInput = $("#name-input").val().trim();
-    var destinationInput = $("#destination-input").val().trim();
-    var firstTrainTimeInput = $("#first-time-input").val().trim();
-    var frequncyInput = $("#frequency-input").val().trim();
+        //Prevent inputs being automatically submitted
+        event.preventDefault();
 
-    $("#name-input").val("");
-    $("#destination-input").val("");
-    $("#first-time-input").val("");
-    $("#frequency-input").val("");
+        //Retrive values from inputs
+        var trainNameInput = $("#name-input").val().trim();
+        var destinationInput = $("#destination-input").val().trim();
+        var firstTrainTimeInput = $("#first-time-input").val().trim();
+        var frequncyInput = $("#frequency-input").val().trim();
 
-    //Calculate time difference
 
-    var timeConverter = moment(firstTrainTimeInput, "HH:mm").subtract(1, "days");
+        console.log(trainNameInput);
+        console.log(destinationInput);
+        console.log(firstTrainTimeInput);
+        console.log(frequncyInput);
+        
 
-    console.log(timeConverter);
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#first-time-input").val("");
+        $("#frequency-input").val("");
 
-    var minuteDiff = moment().diff(timeConverter, "minutes");
+        //Calculate time difference
 
-    console.log("Minutes difference to first train: " + minuteDiff);
+        var timeConverter = moment(firstTrainTimeInput, "HH:mm").subtract(1, "days");
 
-    var timeRemainder = minuteDiff % frequncyInput;
+        console.log(timeConverter);
 
-    // Minute until train
-    var timeToNextTrain = frequncyInput - timeRemainder;
+        var minuteDiff = moment().diff(timeConverter, "minutes");
 
-    console.log("How many minutes left before next train: " + timeToNextTrain);
+        console.log("Minutes difference to first train: " + minuteDiff);
 
-    //Time of next train
-    var nextTrainTimeInput = moment().add(timeToNextTrain, "minutes");
+        var timeRemainder = minuteDiff % frequncyInput;
 
-    //Push input values into an array
-    var inputObject = {
-        trainName: trainNameInput,
-        destination: destinationInput,
-        firstTrainTime: firstTrainTimeInput,
-        frequncy: frequncyInput,
-        nextTrainTime: moment(nextTrainTimeInput).format("HH:mm"),
-        trainArrvingIn: timeToNextTrain
-    }
+        // Minute until train
+        var timeToNextTrain = frequncyInput - timeRemainder;
 
-    console.log(inputObject);
+        console.log("How many minutes left before next train: " + timeToNextTrain);
 
-    //Push input array to Firebase
-    trainData.push(inputObject);
+        //Time of next train
+        var nextTrainTimeInput = moment().add(timeToNextTrain, "minutes");
 
-})
+        //Push input values into an array
+        var inputObject = {
+            trainName: trainNameInput,
+            destination: destinationInput,
+            firstTrainTime: firstTrainTimeInput,
+            frequncy: frequncyInput,
+            nextTrainTime: moment(nextTrainTimeInput).format("HH:mm"),
+            trainArrvingIn: timeToNextTrain
+        }
 
-//Show current time
-function currnetTimeUpdate() {
-    var currentTime = moment().format("HH:mm:ss");
-    $("#time-diaply").text(currentTime);
-}
+        console.log(inputObject);
 
-//Update next train time and next train arriving at in data
-function dataInstantUpdate () {
+        //Push input array to Firebase
+        trainData.push(inputObject);
 
-    $("#train-detail").html("");
-    
-    //Create Firbase event to update page in real-time when value change
-    trainData.on("child_added", function(snapshot){
-    
-        console.log(snapshot.val());
-
-        //Set variables to save values
-        var trainName;
-        var destination;
-        var firstTrainTime;
-        var frequncy;
-
-        var newRow;
-
-        trainName = snapshot.val().trainName;
-        destination = snapshot.val().destination;
-        firstTrainTime = snapshot.val().firstTrainTime;
-        frequncy = snapshot.val().frequncy;
-        nextTrainTime = snapshot.val().nextTrainTime;
-        trainArrvingIn = snapshot.val().trainArrvingIn;
-
-        console.log(trainName);
-        console.log(destination);
-        console.log(firstTrainTime);
-        console.log(frequncy);
-        console.log(nextTrainTime);
-        console.log(trainArrvingIn)
-
-        newRow = $("<tr>").append(
-            $("<td>").text(trainName),
-            $("<td>").text(destination),
-            $("<td>").text(firstTrainTime),
-            $("<td>").text(frequncy + " mins"),
-            $("<td>").text(nextTrainTime),
-            $("<td>").text(trainArrvingIn + " mins"),
-        );
-
-        $("#train-detail").append(newRow);
+        console.log(trainData);
 
     });
 
-    //Calculate time remain and time to next train again
-    var timeConverterInst = moment(firstTrainTime, "HH:mm").subtract(1, "days");
+    // Update next train time and next train arriving at in data
+    function dataInstantUpdate () {
 
-    var minuteDiffInst = moment().diff(timeConverterInst, "minutes");
+        $("#train-detail").html("");
+        
+        // Create Firbase event to update page in real-time when value change
+        database.ref("/train").on("child_added", function(snapshot){
+    
+            console.log(snapshot.val());
 
-    var timeRemainderInst = minuteDiffInst % frequncy;
 
-    var timeToNextTrainInst = frequncy - timeRemainderInst;
+            var newRow;
 
-    database.ref("/train").set({
-        nextTrainTime: timeToNextTrainInst,
-        trainArrvingIn: timeRemainderInst
-    })
-}
+            var trainName = snapshot.val().trainName;
+            var destination = snapshot.val().destination;
+            var firstTrainTime = snapshot.val().firstTrainTime;
+            var frequncy = snapshot.val().frequncy;
+            var nextTrainTime = snapshot.val().nextTrainTime;
+            var trainArrvingIn = snapshot.val().trainArrvingIn;
 
-currnetTimeUpdate();
-setInterval(currnetTimeUpdate, 1000);
-setInterval(dataInstantUpdate, 1000);
+            console.log(trainName);
+            console.log(destination);
+            console.log(firstTrainTime);
+            console.log(frequncy);
+            console.log(nextTrainTime);
+            console.log(trainArrvingIn)
+
+            newRow = $("<tr>").append(
+                $("<td>").text(trainName),
+                $("<td>").text(destination),
+                $("<td>").text(firstTrainTime),
+                $("<td>").text(frequncy + " mins"),
+                $("<td>").text(nextTrainTime),
+                $("<td>").text(trainArrvingIn + " mins"),
+            );
+
+            $("#train-detail").append(newRow);
+
+        });
+
+        //Calculate time remain and time to next train again
+        var timeConverterInst = moment(firstTrainTime, "HH:mm").subtract(1, "days");
+
+        var minuteDiffInst = moment().diff(timeConverterInst, "minutes");
+
+        var timeRemainderInst = minuteDiffInst % frequncy;
+
+        var timeToNextTrainInst = frequncy - timeRemainderInst;
+
+        trainData.push({
+            nextTrainTime: timeToNextTrainInst,
+            trainArrvingIn: timeRemainderInst
+        })
+    }
+
+    //Show current time
+    function currnetTimeUpdate() {
+        var currentTime = moment().format("HH:mm:ss");
+        $("#time-diaply").text(currentTime);
+    }
+
+    currnetTimeUpdate();
+    setInterval(currnetTimeUpdate, 1000);
+    setInterval(dataInstantUpdate, 1000);
+
+});
